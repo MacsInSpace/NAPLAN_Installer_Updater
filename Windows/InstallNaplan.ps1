185a2224dd4b1a54db1294d69304c895386ce919
Craig Hair <craig.hair@gmail.com>
2025-03-05 10:54:12 +1100

    Update InstallNaplan.ps1

--- Windows/InstallNaplan.ps1
+++ Windows/InstallNaplan.ps1
@@ -9,7 +9,7 @@ Start-Transcript -Path "C:\Windows\Temp\NaplanScheduledTask.log" -Append
 # Define the fallback local SMB path (only used if the internet check fails)
 $FallbackSMB = "\\XXXXWDS01\Deploymentshare$\Applications\Naplan.msi"
 $ErrorActionPreference = 'Stop'
-$ForceUpdate = $true #true will force the update regardless of version number
+$ForceUpdate = $false #true will force the update regardless of version number
 
 
 # NAPLAN key dates page
