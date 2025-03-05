308b4e9796867809fa93e24c1e5758f2fd510f40
Craig Hair <craig.hair@gmail.com>
2025-03-05 11:26:25 +1100

    Update NukeScheduledTask.ps1

--- Windows/bin/NukeScheduledTask.ps1
+++ Windows/bin/NukeScheduledTask.ps1
@@ -9,4 +9,16 @@ Start-Transcript -Path "C:\Windows\Temp\NaplanScheduledTask.log" -Append
 $TaskName = "InstallNaplan"
 Unregister-ScheduledTask -TaskName $TaskName -Confirm:$false
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;irm -UseBasicParsing -Uri "https://raw.githubusercontent.com/MacsInSpace/NAPLAN_Installer_Updater/refs/heads/main/Windows/bin/NAPLANnuke.ps1" | iex
+
+$RuleName = "NAPLockedDownBrowserOutbound"
+
+# Check if the rule exists before removing it
+$ruleExists = Get-NetFirewallRule -DisplayName $RuleName -ErrorAction SilentlyContinue
+
+if ($ruleExists) {
+    Remove-NetFirewallRule -DisplayName $RuleName
+    Write-Host "✅ Firewall rule '$RuleName' has been removed."
+} else {
+    Write-Host "⚠️ Firewall rule '$RuleName' does not exist. No action taken."
+}
 Stop-Transcript
