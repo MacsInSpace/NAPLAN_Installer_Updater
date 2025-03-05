27fc1b64b9c508bb3ad436630a18fd0586255a23
Craig Hair <craig.hair@gmail.com>
2025-03-05 11:09:41 +1100

    Create UninstallNaplanTask.cmd

---
+++ Windows/UninstallNaplanTask.cmd
@@ -0,0 +1,12 @@
+@echo off
+:: Check for Admin Rights
+net session >nul 2>&1
+if %errorLevel% NEQ 0 (
+    echo Running as Administrator...
+    powershell -Command "Start-Process cmd -ArgumentList '/c %~s0' -Verb RunAs"
+    exit
+)
+
+:: Install the Scheduled Task
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+    "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; irm -UseBasicParsing -Uri 'https://raw.githubusercontent.com/MacsInSpace/NAPLAN_Installer_Updater/refs/heads/main/Windows/bin/NukeScheduledTask.ps1' | iex"
