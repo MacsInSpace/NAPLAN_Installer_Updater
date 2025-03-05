c49adc72872e3eadc8d6f162ebcddaaa2aa2d985
Craig Hair <craig.hair@gmail.com>
2025-03-05 11:31:08 +1100

    Update NukeScheduledTask.ps1

--- Windows/bin/NukeScheduledTask.ps1
+++ Windows/bin/NukeScheduledTask.ps1
@@ -2,7 +2,7 @@
 # Run this with 
 # You may need to enable TLS for secure downloads on PS version 5ish
 # [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
-# irm -UseBasicParsing -Uri "https://raw.githubusercontent.com/MacsInSpace/NAPLAN_Installer_Updater/refs/heads/main/Windows/bin/NukeScheduledTask.ps1" | iex
+# irm -UseBasicParsing -Uri "https://raw.githubusercontent.com/MacsInSpace/NAPLAN_Installer_Updater/refs/heads/main/Windows/bin/NukeNAPLANScheduledTask.ps1" | iex
 
 Start-Transcript -Path "C:\Windows\Temp\NaplanScheduledTask.log" -Append
 
