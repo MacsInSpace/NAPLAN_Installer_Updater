9f01c3690ac9d20230916c29ec3bbaf18c0f68ae
Craig Hair <craig.hair@gmail.com>
2025-03-07 08:14:43 +1100

    Update NAPLANnuke.ps1

--- Windows/bin/NAPLANnuke.ps1
+++ Windows/bin/NAPLANnuke.ps1
@@ -3,11 +3,11 @@
 # run *THIS* with:
 # You may need to enable TLS for secure downloads on PS version 5ish
 # [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
-# irm -UseBasicParsing -Uri "https://raw.githubusercontent.com/MacsInSpace/NAPLAN_Installer_Updater/refs/heads/testing/Windows/bin/NAPLANnuke.ps1" | iex
+# irm -UseBasicParsing -Uri "https://raw.githubusercontent.com/MacsInSpace/NAPLAN_Installer_Updater/refs/heads/main/Windows/bin/NAPLANnuke.ps1" | iex
 
 Write-Host "Starting Naplan removal process..."
 
-$BranchName = "testing"
+$BranchName = "main"
 
 # Uninstall known MSI versions
 $UninstallGUIDs = @(
