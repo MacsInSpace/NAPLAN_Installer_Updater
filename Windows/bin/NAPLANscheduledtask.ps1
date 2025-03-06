16a97656f6b117a484f42f03562464491f8b695d
Craig Hair <craig.hair@gmail.com>
2025-03-06 19:23:16 +1100

    Update NAPLANscheduledtask.ps1

--- Windows/bin/NAPLANscheduledtask.ps1
+++ Windows/bin/NAPLANscheduledtask.ps1
@@ -4,7 +4,7 @@
 # [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
 # irm -UseBasicParsing -Uri "https://raw.githubusercontent.com/MacsInSpace/NAPLAN_Installer_Updater/refs/heads/testing/Windows/bin/NAPLANscheduledtask.ps1" | iex
 
-Start-Transcript -Path "C:\Windows\Temp\NaplantestingScheduledTask.log" -Append
+#Start-Transcript -Path "C:\Windows\Temp\NaplantestingScheduledTask.log" -Append
 
 # Install NAPLAN Update Scheduled Task
 $TaskName = "InstallNaplan"
@@ -91,5 +91,5 @@ if ($ExistingTask) {
     }
 }
 
-Stop-Transcript
+#Stop-Transcript
 
