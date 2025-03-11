5b888497badaebb19108905ed84eb275ed32d991
Craig Hair <craig.hair@gmail.com>
2025-03-11 16:59:45 +1100

    Update InstallNaplan.ps1

--- Windows/bin/InstallNaplan.ps1
+++ Windows/bin/InstallNaplan.ps1
@@ -330,6 +330,7 @@ if (-not $Installed -and [Environment]::Is64BitOperatingSystem) {
     $Installed = Get-ItemProperty HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* -ErrorAction SilentlyContinue |
         Where-Object { $_.DisplayName -match "NAP Locked Down Browser" }
 }
+
 if ($Installed) {
     Write-Host "Installed Version: $($Installed.DisplayVersion)"
     Write-Host "Installed GUID: $($Installed.PSChildName)"  # GUID of the installed app
@@ -339,10 +340,18 @@ if ($Installed) {
     Write-Host "No InstallLocation property found or NAP Locked Down Browser is not installed."
 }
 
-    # Log it as installed 
-    $currentDateString = Get-Date -Format "yyyyMMdd"
-    $currentDateString | Set-Content -Path $lastUpdateFile -Force
-    
+ # check for exe. If installed and no exe, Uninstall and reinstallno
+    if ($Installed) {
+        $ExePath = Join-Path $Installed.InstallLocation "NAP Locked Down Browser.exe"
+        if (-not (Test-Path $ExePath)) {
+        # Write-Host "NAPLAN installed in bad state. Reinstalling.
+            Start-Process "msiexec.exe" -ArgumentList "/X $Installed.PSChildName /qn /norestart" -NoNewWindow -Wait
+            irm  -UseBasicParsing -Uri "$napnukeurl" | iex
+            $forceinstall = $true
+            $Installed = $null
+        }
+    }
+  
 # Compare versions and proceed only if an update is needed
 if ($ForceUpdate -or $OldVersion -ne $RemoteVersion) {
     # Uninstall old version
@@ -454,7 +463,11 @@ if ($AppPath -and (Test-Path $AppPath)) {
     Write-Host "Installation completed. Cleaning up..."
     Remove-Item "$Setup" -Force -ErrorAction SilentlyContinue -Verbose
     Write-Host "Refreshing icon cache..."
-
+    
+    # Log it as installed 
+    $currentDateString = Get-Date -Format "yyyyMMdd"
+    $currentDateString | Set-Content -Path $lastUpdateFile -Force
+  
     ## Refreshing icon cache option 1
     
     #Write-Host "Stopping Windows Explorer..."
