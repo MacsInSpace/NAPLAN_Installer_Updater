237cb627824058388fac11b870359ca80456747e
Craig Hair <craig.hair@gmail.com>
2025-03-15 18:22:54 +1100

    Update NAPLAN_Launcher.ps1

--- Windows/launcher/NAPLAN_Launcher.ps1
+++ Windows/launcher/NAPLAN_Launcher.ps1
@@ -7,13 +7,75 @@ function Test-NaplanLDBRunning {
 
 # Function to check if system volume is unmuted and set it to 75%
 function Set-Volume {
-    $volume = (Get-VolumeMixer).MasterVolume
-    if ($volume -eq 0) {
-        Write-Host "Unmuting volume..."
-        (Get-VolumeMixer).Mute = $false
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateRange(0, 100)]
+        [int]$vol
+    )
+
+    # Convert percentage to scalar value (0.0 to 1.0)
+    $volumeLevel = [math]::Round($vol / 100, 2)
+
+    # Check if the type is already defined to prevent duplicate addition
+    if (-not ("Audio" -as [type])) {
+        $code = @"
+using System;
+using System.Runtime.InteropServices;
+
+[Guid("5CDF2C82-841E-4546-9722-0CF74078229A"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IAudioEndpointVolume {
+    int f(); int g(); int h(); int i();
+    int SetMasterVolumeLevelScalar(float fLevel, System.Guid pguidEventContext);
+    int j();
+    int GetMasterVolumeLevelScalar(out float pfLevel);
+    int k(); int l(); int m(); int n();
+    int SetMute([MarshalAs(UnmanagedType.Bool)] bool bMute, System.Guid pguidEventContext);
+    int GetMute(out bool pbMute);
+}
+
+[Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IMMDevice {
+    int Activate(ref System.Guid id, int clsCtx, int activationParams, out IAudioEndpointVolume aev);
+}
+
+[Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IMMDeviceEnumerator {
+    int f();
+    int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice endpoint);
+}
+
+[ComImport, Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")] class MMDeviceEnumeratorComObject { }
+
+public class Audio {
+    static IAudioEndpointVolume Vol() {
+        var enumerator = new MMDeviceEnumeratorComObject() as IMMDeviceEnumerator;
+        IMMDevice dev = null;
+        Marshal.ThrowExceptionForHR(enumerator.GetDefaultAudioEndpoint(0, 1, out dev));
+        IAudioEndpointVolume epv = null;
+        var epvid = typeof(IAudioEndpointVolume).GUID;
+        Marshal.ThrowExceptionForHR(dev.Activate(ref epvid, 23, 0, out epv));
+        return epv;
     }
-    Write-Host "Setting volume to 75%..."
-    (Get-VolumeMixer).MasterVolume = 75
+
+    public static float Volume {
+        get { float v = -1; Marshal.ThrowExceptionForHR(Vol().GetMasterVolumeLevelScalar(out v)); return v; }
+        set { Marshal.ThrowExceptionForHR(Vol().SetMasterVolumeLevelScalar(value, System.Guid.Empty)); }
+    }
+
+    public static bool Mute {
+        get { bool mute; Marshal.ThrowExceptionForHR(Vol().GetMute(out mute)); return mute; }
+        set { Marshal.ThrowExceptionForHR(Vol().SetMute(value, System.Guid.Empty)); }
+    }
+}
+"@
+        Add-Type -TypeDefinition $code -Language CSharp -PassThru | Out-Null
+    }
+
+    # Unmute and set volume
+    [Audio]::Mute = $false
+    [Audio]::Volume = $volumeLevel
+
+    Write-Host "Volume set to $vol%"
 }
 
 # Function to check internet connectivity
@@ -56,21 +118,12 @@ function Prompt-Headphones {
 }
 
 # MAIN SCRIPT EXECUTION
-Add-Type -TypeDefinition @"
-using System;
-using System.Runtime.InteropServices;
-public class VolumeMixer {
-    [DllImport("user32.dll")]
-    public static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
-}
-"@ -Name "VolumeMixer" -Namespace "System"
-
 if (Test-NaplanLDBRunning) {
     Write-Host "NAPLAN LDB is already running."
     exit 1
 }
 
-Set-Volume
+Set-Volume -vol 75
 if (-not (Test-Internet)) { exit 1 }
 if (-not (Test-BatteryLevel)) { exit 1 }
 if (-not (Test-Resolution)) { exit 1 }
